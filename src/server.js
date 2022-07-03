3f2c9b5b784145e68deeb8b5e0e3980adbb8f0a3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 17:13:30 +0900

    config 7

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine"
+import initWebRoutes from "./route/web"
+
+// instance of express
+let app = express()
+
+// config app
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+viewEngine(app)
+initWebRoutes(app)
