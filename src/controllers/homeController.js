06d931ca12b4ec5725431616b0065c2a0bada608
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 23:08:11 +0900

    Edit func edit data database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -25,10 +25,34 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log(data);
     return res.render("displayCRUD.ejs", {
         dataTable: data
     });
+};
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render("editCRUD.ejs", {
+            user: userData
+        });
+    } else {
+        return res.send("User not found");
+    }
+
+
+    return res.send("Hello edit page");
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    console.log(allUsers);
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUsers
+    });
+
 };
 
 module.exports = {
@@ -36,4 +60,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 };
