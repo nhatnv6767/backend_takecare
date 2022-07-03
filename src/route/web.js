7ac9803b3f94affcd3a259a456bd1b485b351803
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 18:52:42 +0900

    #18

--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,12 @@
 import express from "express";
+import homeController from "../controllers/homeController"
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello, world!");
+    router.get("/", homeController.getHomePage)
+    router.get('/about', (req, res) => {
+        return res.send("How about you today")
     })
 
     return app.use("/", router)
