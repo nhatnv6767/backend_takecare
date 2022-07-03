42c9cdbf5cee559e1a86e8dd719c8ef4253dead1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 18:13:33 +0900

    :@:@:@:@@:@:@:@:@: \\\\\\\\

--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,10 @@
-import express from "express"
+import express from "express";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) =>{
-        return res.send("Hello world")
+    router.get("/", (req, res) => {
+        return res.send("Hello, world!");
     })
     return app.use("/", router)
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRoutes from "./route/web"
