54150fdb611fc676b1c010c70a0f8872e7bc8bd5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 18:18:05 +0900

    11111111111111111111

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
 import express from "express";
+// lấy được các tham số từ client gửi lên
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRoutes from "./route/web"
+require('dotenv').config();
 
-require("dotenv").config()
-
-// instance of express
+// instance cua express
 let app = express()
 
-// config app
-
+//config app
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app)
 initWebRoutes(app)
 
-let port = process.env.PORT
-app.listen(port, ()=> {
-    console.log("Backend NodeJS is running on port " + port)
+let port = process.env.PORT;
+app.listen(port, () => {
+    //callback
+    console.log("Backend NodeJS is running on the port: " + port)
 })
